13d2bde6a26bfbdd27bbc13138ae656d6ac59f0c
Koodi Maailm <142037310+Koodimaailm@users.noreply.github.com>
2023-11-20 15:00:32 +0200

    Update postfixmail.sh

--- postfixmail.sh
+++ postfixmail.sh
@@ -2,12 +2,12 @@
 
 
 
-postconf - e "relayhost = [smtp.gmail.com]:587"
-postconf - e "smtp_sasl_auth_enable = yes"
-postconf - e "smtp_sasl_security_options = noanonymous"
-postconf - e "smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd"
-postconf - e "smtp_use_tls = yes"
-postconf - e "smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt"
+postconf -e "relayhost = [smtp.gmail.com]:587"
+postconf -e "smtp_sasl_auth_enable = yes"
+postconf -e "smtp_sasl_security_options = noanonymous"
+postconf -e "smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd"
+postconf -e "smtp_use_tls = yes"
+postconf -e "smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt"
 
 
 echo "[smtp.gmail.com]:587 USERNAME:PASSWORD" > /etc/postfix/sasl_passwd
